cd13290ffb47eb7b0e42e689ab85877d44872413
Jonathan Stirling <phoenixrises@hotmail.co.uk>
2011-08-31 16:29:21 +0100

    Deleted redundant views

--- classes/mvc/views/boot_FrontPageView.php
+++
@@ -1,6 +0,0 @@
-<?php
-class boot_FrontPageView extends boot_TemplateView {
-	protected function goInnerBodyContent() {
-		pfl('<p>The current time is <span class="date">%s</span></p>', $this->get_date);	
-	}
-}
