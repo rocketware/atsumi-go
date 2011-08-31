b523b18763791a011d620f41ce56d2a71fe5ddc0
Jonathan Stirling <phoenixrises@hotmail.co.uk>
2011-08-31 16:28:50 +0100

    No idea how to use git atm

---
+++ classes/app/ago_Settings.php
@@ -0,0 +1,21 @@
+<?php
+class ago_Settings extends atsumi_AbstractAppSettings {
+	/* we can setup base settings here, can be useful for version numbers etc */
+	protected $settings = 	array (
+					'debug'		=> true,
+					'cli'		=> true
+				);
+	/* 	At times you may want to utilise the construct, this could be to 
+	 * 	analyse what domain a user is on and you could give them an 
+	 * 	alternate specification for example */
+	public function __construct () {}
+	
+	/* the specification is how atsumi knows what URI's call what class & method */
+	public function init_specification () {
+		return array (	
+			''	=> 'boot_FrontPageController',
+			'ajax'	=> 'boot_AjaxController'
+		);
+	}
+}
+?>
