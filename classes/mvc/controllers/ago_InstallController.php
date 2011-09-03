e8ae1d7d51f635410430789e6e52fee548e99aea
Jonathan Stirling <jonny@bellerophon.(none)>
2011-09-03 21:43:07 +0100

    Start switch implementation for install

--- classes/mvc/controllers/ago_InstallController.php
+++ classes/mvc/controllers/ago_InstallController.php
@@ -14,15 +14,30 @@ class ago_InstallController extends mvc_AbstractController {
 			throw new app_InvalidUsageException();
 		}
 
-		
+		while($arg = array_shift($allArgs)) {
+			switch($arg) {
+				case '-c':
+					$this->goConfigureForCLI();
+					break;
+				case '-d':
+					$type = array_shift($allArgs);
+					$this->goConfigureForDb($type);
+					break;
+				default:
+					throw new app_InvalidUsageException();
+			}
+		}
 	}
 
 	private function goConfigureForCLI() {
-
+		echo 'cli';
 	}
 
 	private function goConfigureForDb($dbType) {
-		
+		if(!in_array($dbType, array('postgresql', 'mysql'))) {
+			throw new Exception('Unknown database type');
+		}
+		echo $dbType;
 	}
 }
 ?>
