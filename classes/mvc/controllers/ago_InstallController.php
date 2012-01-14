6e7f2c83abb84d31ff9ec0bdc101ccd146539add
Jonny Stirling <deathofphoenix@gmail.com>
2012-01-14 23:07:00 +0100

    Get project from git, rename files and replace namespace with provided names

--- classes/mvc/controllers/ago_InstallController.php
+++ classes/mvc/controllers/ago_InstallController.php
@@ -9,11 +9,9 @@ class ago_InstallController extends mvc_AbstractController {
 		
 		$projectFolder = array_pop($allArgs);
 		$namespace = array_pop($allArgs);
-
-		if(preg_match('/^[^A-z0-9]+/', $projectFolder) || preg_match('/^[^A-z0-9]+/', $namespace)) {
+		if(preg_match('/^[^A-z0-9_\/]+$/', $projectFolder) || preg_match('/^[^A-z0-9]+/', $namespace)) {
 			throw new app_InvalidUsageException();
 		}
-
 		while($arg = array_shift($allArgs)) {
 			switch($arg) {
 				case '-c':
@@ -27,6 +25,36 @@ class ago_InstallController extends mvc_AbstractController {
 					throw new app_InvalidUsageException();
 			}
 		}
+
+		// We should already have atsumi, otherwise we wouldn't be here.
+		// So, get the project base git repo and stick it in the dir we chose
+		$gitOutput = array();
+		$gitReturn = null;
+		pf("Getting latest version of the Atsumi Base Project...");
+		exec(sprintf('git clone -q git@github.com:phoenixrises/atsumi-project-base.git %s', $projectFolder), $gitOutput, $gitReturn);
+
+		if($gitReturn !== 0) {
+			// Git failed
+			throw new Exception("Git failed to pull the project base");
+		}
+		pfl("Done.\n");
+
+		// Scape every file added
+		pf("Finding project files....");
+		$dirListing = $this->scandir_r($projectFolder);
+		pfl("found %s files\n", count($dirListing));
+
+		// Current default name space is boot
+		foreach($dirListing as $file) {
+			$fileContents = file_get_contents($file);
+			$replacementCount = 0;
+			$fileContents = str_replace('boot', $namespace, $fileContents, $replacementCount);
+			file_put_contents($file, $fileContents);
+		       	pfl('Replaced %d items in %s', $replacementCount, $file);
+			pf('Moving file...');
+			rename($file, str_replace('boot', $namespace, $file));
+			pfl('Done.');	
+		}
 	}
 
 	private function goConfigureForCLI() {
@@ -38,6 +66,26 @@ class ago_InstallController extends mvc_AbstractController {
 			throw new Exception('Unknown database type');
 		}
 		echo $dbType;
+	}
+
+	private function scandir_r($folder) {
+		$listing = scandir($folder);
+		$data = array();
+		foreach($listing as $item) {
+			if($item == '.' || $item == '..' || $item == '.git') {
+				continue; 
+			}
+			if(is_dir(sprintf('%s/%s', $folder, $item))) {
+				$downStream = $this->scandir_r(sprintf('%s/%s', $folder, $item));
+
+				$data = array_merge($data, $downStream);
+			} else if(is_file(sprintf('%s/%s', $folder, $item))) {
+				$data[] = sprintf('%s/%s', $folder, $item);
+			} else {
+				// No idea wtf this is...
+			}
+		}
+		return $data;
 	}
 }
 ?>
